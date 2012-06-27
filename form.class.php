1ee40dbdbfcf8e6760d7e7fb1a89d78aa71c8483
Will Washburn <will@winston>
2012-06-27 12:25:07 -0400

    put html class into its own file and included from form class

--- form.class.php
+++ form.class.php
@@ -34,7 +34,10 @@
      *
      */
 
+
 namespace formstrap;
+
+require_once 'html.clss.php';
 
 //WWBN allow different enctypes
 //WBN create quick forms like "search box" or "email_pw_login" w/ different options
@@ -565,93 +568,5 @@ class submit extends button
 }
 
 
-class html
-{
-    protected $class, $style;
 
-    /*
-    * Create attribute string
-    * @author Will
-    *
-    * @return string of the attribute
-    * eg  class = "classname otherclass"
-    *
-    * add ability to overwrite attribute name in string
-    * eg change "form_class" to just "class"
-    *
-    */
-    protected function attributize($attribute, $data = FALSE)
-    {
-        if (!$data) {
-            if (isset($this->$attribute)) {
-                $data = $this->$attribute;
-            } else {
-                $data = FALSE;
-            }
-        }
-
-        if ($data) {
-            $string_to_return = ' ' . $attribute . '="';
-            if (is_string($data)) {
-                $string_to_return .= $data;
-            } elseif (is_array($data)) {
-                foreach ($data as $key => $value) {
-                    $string_to_return .= ' ' . $value;
-                }
-            } else {
-                $this->$attribute = '';
-            }
-            $string_to_return .= '"';
-            $this->$attribute = $string_to_return;
-        } else {
-            $this->$attribute = '';
-        }
-    }
-
-    /*
-     * Add attribute
-     * @oa	Will
-     */
-    protected function add_attribute($attribute_name, $attribute)
-    {
-        if (is_array($this->$attribute_name)) {
-            $add = array($attribute => $attribute);
-            $this->array_push_associative($this->$attribute_name, $add);
-        } else {
-            $this->$attribute_name = array($attribute => $attribute);
-        }
-    }
-
-    /*
-     * Remove attribute
-     *
-     */
-    protected function remove_attribute($attribute_name, $attribute)
-    {
-        if (isset($this->$attribute_name[$attribute])) {
-            unset($this->$attribute_name[$attribute]);
-        }
-    }
-
-
-    /*
-     * Array push
-     * @author Will
-     *
-     * push onto array with associations
-     */
-    private function array_push_associative(&$arr)
-    {
-        $args = func_get_args();
-        foreach ($args as $arg) {
-            if (is_array($arg)) {
-                foreach ($arg as $key => $value) {
-                    $arr[$key] = $value;
-                }
-            } else {
-                $arr[$arg] = "";
-            }
-        }
-    }
-}
 
---
+++ html.class.php
@@ -0,0 +1,100 @@
+<?php
+
+    /*
+     * Abstract Html Class
+     * @author Will
+     *
+     * when we are creating html elements, this is a good base class
+     *
+     */
+   namespace formstrap;
+
+    class html
+    {
+        protected $class, $style;
+
+        /*
+        * Create attribute string
+        * @author Will
+        *
+        * @return string of the attribute
+        * eg  class = "classname otherclass"
+        *
+        * //WWBN add ability to overwrite attribute name in string
+        * eg change "form_class" to just "class"
+        *
+        */
+        protected function attributize($attribute, $data = FALSE)
+        {
+            if (!$data) {
+                if (isset($this->$attribute)) {
+                    $data = $this->$attribute;
+                } else {
+                    $data = FALSE;
+                }
+            }
+
+            if ($data) {
+                $string_to_return = ' ' . $attribute . '="';
+                if (is_string($data)) {
+                    $string_to_return .= $data;
+                } elseif (is_array($data)) {
+                    foreach ($data as $key => $value) {
+                        $string_to_return .= ' ' . $value;
+                    }
+                } else {
+                    $this->$attribute = '';
+                }
+                $string_to_return .= '"';
+                $this->$attribute = $string_to_return;
+            } else {
+                $this->$attribute = '';
+            }
+        }
+
+        /*
+        * Add attribute
+        * @oa	Will
+        */
+        protected function add_attribute($attribute_name, $attribute)
+        {
+            if (is_array($this->$attribute_name)) {
+                $add = array($attribute => $attribute);
+                $this->array_push_associative($this->$attribute_name, $add);
+            } else {
+                $this->$attribute_name = array($attribute => $attribute);
+            }
+        }
+
+        /*
+        * Remove attribute
+        *
+        */
+        protected function remove_attribute($attribute_name, $attribute)
+        {
+            if (isset($this->$attribute_name[$attribute])) {
+                unset($this->$attribute_name[$attribute]);
+            }
+        }
+
+
+        /*
+        * Array push
+        * @author Will
+        *
+        * push onto array with associations
+        */
+        private function array_push_associative(&$arr)
+        {
+            $args = func_get_args();
+            foreach ($args as $arg) {
+                if (is_array($arg)) {
+                    foreach ($arg as $key => $value) {
+                        $arr[$key] = $value;
+                    }
+                } else {
+                    $arr[$arg] = "";
+                }
+            }
+        }
+    }
